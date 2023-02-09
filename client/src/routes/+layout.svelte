123faf4f35e41b9e85fc4f05bc0bd14ed980f59a
LoganW <logan.willaumez@gmail.com>
2023-02-09 08:13:48 +0100

    feat(home): add first design with tailwind and scss global + theme switcher

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Switch from '../components/Switch.svelte';
+    import { theme } from '$lib/stores/theme';
+    import '$lib/styles/main.scss';
+</script>
+
+<style global lang="scss">
+    .main-container{
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+        padding: 40px 20px 40px 20px;
+    }
+</style>
+
+<svelte:head>
+    <meta name="color-scheme" content={$theme}/>
+    <link rel="stylesheet" href={`/themes/${$theme}.scss`} />
+</svelte:head>
+
+<main class="main-container">
+    <Switch />
+    <slot/>
+</main>
