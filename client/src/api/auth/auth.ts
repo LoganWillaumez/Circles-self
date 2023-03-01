2a7867318da546c709b12dfc0970653d34ead811
LoganW <logan.willaumez@gmail.com>
2023-03-01 20:36:08 +0100

    feat(signup): add mail page

--- client/src/api/auth/auth.ts
+++ client/src/api/auth/auth.ts
@@ -1,10 +1,11 @@
-import { fail, type Actions } from "@sveltejs/kit";
+import { error, fail, type Actions } from "@sveltejs/kit";
 import { authenthificationSchema } from "$lib/schema/authentification";
 import API from "../Api";
 import { validateData } from "$lib/schema/validation";
+import {isAxiosError} from "axios";
 
 export const authentification: Actions = {
-    signup: async ({request} : any) => {
+    signup: async ({request}) => {
         const {formData, errors} = await validateData( await request.formData(), authenthificationSchema.registerSchema); 
         if (errors) {
           return fail(400, {
@@ -14,9 +15,17 @@ export const authentification: Actions = {
         }
         try{
             const response = await API.post("auth/signup", formData);
-            return response; 
-        } catch(err: any) {
-            return fail(err.response.status, {message: err.response.data.message});
+            return response;
+        } catch(err) {
+            if(isAxiosError(err)){
+              if(err.response){
+                return fail(err.response.status, {message: err.response.data.message});
+              } else {
+                return error(500);
+              }
+            } else {
+              return error(500);
+            }
         }
     }
 }
