931946fc1d00f7b5d620c27e10415b8c6ee681b6
LoganW <logan.willaumez@gmail.com>
2023-03-02 07:39:12 +0100

    feat(mail): add begin mail idea (WIP)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="container">
     <img class="w-full grow" alt="Logo of Circle" src="/Circle_logo.svg" />
     <div class="flex flex-col w-full gap-[15px]">
-        <Button text="Sign up"/>
+        <Button text="Sign up" href="signup"/>
         <Button text="Sign in"/>
     </div>
 </div>
