ea54607b02cdfce2763cc0bfd62086c58c947be3
LoganW <logan.willaumez@gmail.com>
2023-03-12 14:00:47 +0100

    feat(design): Change wrapper and scroll for better design (responsive)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,9 +3,13 @@
 </script>
 
 <div class="container">
-    <img class="w-full grow" alt="Logo of Circle" src="/Circle_logo.svg" />
-    <div class="flex flex-col w-full gap-[15px]">
-        <Button text="Sign up" href="signup"/>
-        <Button text="Sign in" href="signin"/>
+    <div class="scroll_wrapper">
+        <div>
+            <img class="w-full grow max-w-[400px] sm:flex-grow-0" alt="Logo of Circle" src="/Circle_logo.svg" />
+            <div class="flex flex-col w-full gap-[15px] items-center">
+                <Button text="Sign up" href="signup"/>
+                <Button text="Sign in" href="signin"/>
+            </div>
+        </div>
     </div>
 </div>
