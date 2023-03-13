f12827abb595a4c98b7634207e91e045cd977afe
LoganW <logan.willaumez@gmail.com>
2023-03-13 13:23:40 +0100

    feat(lang): Add multi-language capability

---
+++ client/src/lib/components/LangSwitch.svelte
@@ -0,0 +1,48 @@
+<style lang="scss">
+    .switch {
+      border-radius: 83px;
+      width: 50px;
+      height: 25px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      background-color: var(--fill-reverse);
+      color: var(--text-reverse);
+      position: absolute;
+      top: 20px;
+      left: 20px;
+      .switch-state{
+        position: absolute;
+        transition: all 300ms ease-in-out;
+        right: 5px;
+      }
+      .switch-moon{
+        position: absolute;
+        transition: all 300ms ease-in-out;
+        left: 5px;
+      }
+      &--dark{
+        .switch-state{
+          transform: translateX(0px);
+        }
+        .switch-moon{
+          transform: translateX(0px);
+        }
+      }
+      &--light{
+        .switch-state{
+          transform: translateX(-25px);
+        }
+        .switch-moon{
+          transform: translateX(25px);
+        }
+      }
+    }
+  </style>
+  
+  <script>
+    import { invalidateAll } from '$app/navigation';
+  import { LL } from '$lib/i18n/i18n-svelte';
+  </script>
+  
+  <a class="switch" on:click={invalidateAll} href="/?lang={$LL.SWITCH_LANGUAGE()}"> {$LL.SWITCH_LANGUAGE()}</a>
--- client/src/routes/[[lang=langCode]]/+page.svelte
+++ client/src/routes/[[lang=langCode]]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from '$lib/components/Button.svelte';
+    import { LL } from '$lib/i18n/i18n-svelte';
 </script>
 
 <div class="container">
@@ -7,7 +8,7 @@
         <div>
             <img class="w-full grow max-w-[400px] sm:flex-grow-0" alt="Logo of Circle" src="/Circle_logo.svg" />
             <div class="flex flex-col w-full gap-[15px] items-center">
-                <Button text="Sign up" href="signup"/>
+                <Button text="{$LL.SIGNUP()}" href="signup"/>
                 <Button text="Sign in" href="signin"/>
             </div>
         </div>
