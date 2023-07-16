c368061a883181680b8ea57b6b0c399526f29ac8
LoganW <logan.willaumez@gmail.com>
2023-07-16 08:17:30 +0200

    modify element for vercel

--- @app/client/svelte.config.js
+++ @app/client/svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import {vitePreprocess} from '@sveltejs/kit/vite';
 import { Server } from 'socket.io'
+import sveltePreprocess from 'svelte-preprocess';
 
 
 const webSocketServer = {
@@ -21,7 +22,7 @@ const webSocketServer = {
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter(),
   },
