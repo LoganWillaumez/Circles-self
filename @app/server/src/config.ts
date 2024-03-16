e80816f476538e0d351a66d78eec37cb0fd0d51a
LoganW <logan.willaumez@gmail.com>
2024-03-16 07:19:22 +0100

    Type: feat Title: default values Description: adapt config with default values

--- @app/server/src/config.ts
+++ @app/server/src/config.ts
@@ -2,11 +2,11 @@ import {Config} from './types/Config';
 
 const devConfig: Config = {
   db: {
-    host: process.env.DEV_DB_HOST || '',
-    user: process.env.DEV_DB_USER || '',
-    port: +process.env.DEV_DB_PORT! || 5432,
-    password: process.env.DEV_DB_PASSWORD || '',
-    database: process.env.DEV_DB_NAME || ''
+    host: process.env.DEV_DB_HOST || 'localhost',
+    user: process.env.DEV_DB_USER || 'postgres',
+    port: +process.env.DEV_DB_PORT! || 4101,
+    password: process.env.DEV_DB_PASSWORD || 'postgres',
+    database: process.env.DEV_DB_NAME || 'circles'
   },
   token: {
     accessTokenLife: 10 * 60 * 60 * 1000, // 10 minutes
